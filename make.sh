0affde5c36988864be6fbdf89c227943a329db16
hostingmydata <93928675+hostingmydata@users.noreply.github.com>
2021-12-08 15:20:35 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -91,7 +91,7 @@ fi
       c=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       d=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       e=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
-      echo $network:$a:$b:$c:$d:$e >> ip.list #Для /64 сети требуется 4 блока:a, b, c, d #Если сеть /48, то 5 блоков, то есть + e блок через двоеточие
+      echo $network:$a:$b:$c:$d >> ip.list #Для /64 сети требуется 4 блока:a, b, c, d #Если сеть /48, то 5 блоков, то есть + e блок через двоеточие
   }
 
   #echo "$MAXCOUNT случайных IPv6:"
